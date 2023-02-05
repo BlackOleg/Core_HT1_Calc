ebed0b19f3e7bf148598875e69368b2c1a0d3000
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-02-05 21:12:01 +0400

    Initial commit

---
+++ src/CalcExeption.java
@@ -0,0 +1,7 @@
+public class CalcExeption extends Exception  {
+
+    public CalcExeption() { super(); }
+    public CalcExeption(String message) { super(message); }
+    public CalcExeption(String message, Throwable cause) { super(message, cause); }
+    public CalcExeption(Throwable cause) { super(cause); }
+}
